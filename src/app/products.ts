2aebc41e28d5d25ccbd2b5d689f0ed558f2ecfdc
giorgiocl <geovanny.campoverde@thoughtworks.com>
2023-08-01 21:30:35 -0500

    @giorgiocl: Updating code

--- src/app/products.ts
+++ src/app/products.ts
@@ -23,6 +23,12 @@ export const products = [
     name: 'Phone Standard',
     price: 299,
     description: ''
+  },
+  {
+    id: 4,
+    name: 'New product',
+    price: 299,
+    description: ''
   }
 ];
 
